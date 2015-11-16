9c4f72681769e96d660b68124f2bcceb1761bde3
Ten Wong <wangtengoo7@gmail.com>
2015-11-16 16:39:34 +0800

    Proved: no-block

--- pion_one_execute.py
+++ pion_one_execute.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # copyright 2015 seeed, wangtengoo7@gmail.com
-
 import sys
 import requests
+import threading
 
 red_led_on_api = "https://cn.iot.seeed.cc/v1/node/GroveRelay/onoff/1?access_token=14e9888a13b1d9b6c1e7d66ef364d1f6"
 red_led_off_api = "https://cn.iot.seeed.cc/v1/node/GroveRelay/onoff/0?access_token=14e9888a13b1d9b6c1e7d66ef364d1f6"
@@ -11,20 +11,20 @@ green_led_off_api = "https://cn.iot.seeed.cc/v1/node/GroveRelay_02/onoff/0?acces
 orange_led_on_api = "https://cn.iot.seeed.cc/v1/node/GroveRelay_01/onoff/1?access_token=14e9888a13b1d9b6c1e7d66ef364d1f6"
 orange_led_off_api = "https://cn.iot.seeed.cc/v1/node/GroveRelay_01/onoff/0?access_token=14e9888a13b1d9b6c1e7d66ef364d1f6"
 
+def post_url(url):
+    requests.post(url)
+
 def main(argv):
     status = argv
     if status == "success":
-        requests.post(red_led_off_api,verify=False)
-        requests.post(green_led_on_api,verify=False)
-        requests.post(orange_led_off_api,verify=False)
+        urls =[red_led_off_api, orange_led_off_api, green_led_on_api]
     elif status == "failure":
-        requests.post(green_led_off_api,verify=False)
-        requests.post(red_led_on_api,verify=False)
-        requests.post(orange_led_off_api,verify=False)
+        urls =[red_led_on_api, orange_led_off_api, green_led_off_api]
     elif status == "building":
-        requests.post(orange_led_on_api,verify=False)
-        requests.post(red_led_off_api,verify=False)
-        requests.post(green_led_off_api,verify=False)
+        urls =[red_led_off_api, orange_led_on_api, green_led_off_api]
+
+    for url in urls:
+        threading.Thread(target=post_url, args=(url,)).start()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
