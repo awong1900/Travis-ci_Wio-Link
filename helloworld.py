cd9e8a7f8c00c4a6a62f7b9b7046b6c44ddf4aee
Ten Wong <wangtengoo7@gmail.com>
2015-11-19 11:39:44 +0800

    add

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,8 @@
 import time
 
 def main():
-    print "helloword, Are you kid me ~"
-    time.sleep(3)
-    error
+    print "helloword, Are you kid me ~
+    # time.sleep(3)
 
 if __name__ == "__main__":
     main()
